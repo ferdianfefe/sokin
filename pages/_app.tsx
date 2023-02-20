c8ad30adae503d8626427425d6f999de7ba17289
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-02-20 21:32:22 +0700

    feat: add codebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import routerProvider from "@pankod/refine-nextjs-router";
 import dataProvider from "@pankod/refine-simple-rest";
 import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
 
+import "src/styles/globals.css";
+
 const API_URL = "https://api.fake-rest.refine.dev";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
