07ca87a62281024689f8fdcf571a013d942fcaeb
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-02-20 21:24:34 +0700

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-nextjs-router";
+import dataProvider from "@pankod/refine-simple-rest";
+import { HeadlessInferencer } from "@pankod/refine-inferencer/headless";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider(API_URL)}
+      resources={[
+        {
+          name: "posts",
+          list: HeadlessInferencer,
+          edit: HeadlessInferencer,
+          show: HeadlessInferencer,
+          create: HeadlessInferencer,
+          canDelete: true,
+        },
+      ]}
+    >
+      <Component {...pageProps} />
+    </Refine>
+  );
+}
+
+export default MyApp;
