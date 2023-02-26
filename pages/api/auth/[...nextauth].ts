fd5e5b1d1f6ec464391ec738ffd8b850472a31f2
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-02-26 19:21:35 +0700

    Feature/setup pwa (#6)
    * feat: integrate prisma & add next-uath google provider
    
    * feat: integrate google provider
    
    * fix: moved global.css
    
    * feat: set up PWA

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,14 @@
 import { NextApiHandler } from "next";
-import NextAuth from "next-auth";
+import NextAuth, { Awaitable } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../lib/prisma";
 
-const authHander: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHander;
-
 const options = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
       authorization: {
         params: {
           prompt: "consent",
@@ -21,10 +18,10 @@ const options = {
       },
       profile(profile, tokens) {
         return {
-          id: profile.id.toString(),
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          image: profile.photos[0].value,
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
         };
       },
     }),
@@ -32,22 +29,57 @@ const options = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
   callbacks: {
-    async signIn(user: any, account: any, profile: any) {
-      console.log(user, account, profile);
-      const { email } = user;
+    async signIn({ user, account }: { user: any; account: any }) {
+      const { userEmail } = user;
+
       const userExists = await prisma.user.findUnique({
         where: {
-          email,
+          email: userEmail,
         },
       });
+
       if (!userExists) {
         await prisma.user.create({
           data: {
-            email,
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            accounts: {
+              create: {
+                id: account.id,
+                provider: account.provider,
+                providerAccountId: account.id,
+                refreshToken: account.refreshToken,
+                accessToken: account.accessToken,
+                expiresAt: account.accessTokenExpires,
+              },
+            },
           },
         });
+        return true;
       }
+
       return true;
     },
   },
 };
+
+const authHandler: NextApiHandler = (req, res) =>
+  NextAuth(req, res, { ...options });
+export default authHandler;
+
+// id            String    @id @db.ObjectId @map("_id")
+// name          String?
+// email         String    @unique
+// emailVerified DateTime? @map("email_verified")
+// password      String?   @map("hashed_password")
+// image         String?
+// accounts      Account[]
+// sessions      Session[]
+// balance       Float     @default(0)
+// coordinates   String?
+// points        Int       @default(0)
+// creditScore   Int       @default(0)
+// createdAt     DateTime  @default(now())
+// updatedAt     DateTime  @updatedAt
