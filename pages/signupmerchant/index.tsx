081b9e98eb1dc6bb1949b1728ea2a8381fbb15f2
Nur Aziz Tri Indrawan <indraaziznur@gmail.com>
2023-02-28 21:44:30 +0700

    feature: add signup merchant

---
+++ pages/signupmerchant/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SignupMerchant() {
+  return (
+    <>
+        <div className="relative flex-col items-center flex bg-[#F37D27]/25 w-full min-h-screen">
+            <div className='mt-20 flex flex-col justify-center items-center'>
+              <h3>Daftarkan Usahamu,</h3>
+              <h3>Jadilah Bagian dari <span className='font-bold'>Sokin</span></h3>
+            </div>
+            <div className='p-8 bg-white h-[450px] w-[90%] rounded-3xl'>
+
+            </div>
+        </div>
+    </>
+  )
+}
