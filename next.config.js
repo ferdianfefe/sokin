e6f113f089d857bf9f1dfac51ed16cff29419666
Nur Aziz Tri Indrawan <88185134+Poor1ST@users.noreply.github.com>
2023-02-28 16:07:08 +0700

    Feature/add navbar (#7)
    * feature:add navbar
    
    baru mbuat folder, codingnya belum
    
    * fix:enhance navbar
    
    * fix:navbar logo
    
    * fix:navbar
    
    nambahin dikit
    
    ---------
    
    Co-authored-by: Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -6,8 +6,26 @@ const withPWA = require("next-pwa")({
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
   experimental: {
     newNextLinkBehavior: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        // test: /\.(js|ts)x?$/,
+       // for webpack 5 use
+       and: [/\.(js|ts)x?$/]
+      },
+      
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 });
 
