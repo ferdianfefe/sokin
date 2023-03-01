17ef860fb42a83c88cfc628c22f7591cc3280384
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-03-01 09:17:27 +0700

    Feature/add signup page (#16)
    * feature:add signup page
    
    * feat: add react hook form
    
    * feat: integrated react hook form
    
    ---------
    
    Co-authored-by: Nur Aziz Tri Indrawan <indraaziznur@gmail.com>

--- pages/api/signup/user/index.ts
+++ pages/api/signup/user/index.ts
@@ -1,23 +1,24 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, Prisma } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type SignupRequestBody = Omit<Prisma.OwnerCreateInput, 'id'> & { name?: string };
+type SignupRequestBody = Omit<Prisma.OwnerCreateInput, "id"> & {
+  name?: string;
+};
 
 const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { name, email, password } = req.body as SignupRequestBody;
     const user = await prisma.user.findUnique({
       where: { email },
     });
 
     if (user) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     const newUser = await prisma.user.create({
@@ -27,10 +28,10 @@ export default async function handler(
     return res.status(201).json(newUser);
   }
 
-  if (req.method === 'GET'){
-    const users = await prisma.user.findMany()
+  if (req.method === "GET") {
+    const users = await prisma.user.findMany();
     return res.status(200).json(users);
   }
 
-  return res.status(405).json({ message: 'Method not allowed' });
+  return res.status(405).json({ message: "Method not allowed" });
 }
