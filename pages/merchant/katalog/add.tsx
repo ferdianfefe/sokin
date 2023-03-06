69e75d0d2324743c703a108330adbc2ea0d4420e
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-03-06 14:51:33 +0700

    Feature/add menu api (#29)
    * feature:add signup page
    
    * feat: add react hook form
    
    * feat: integrated react hook form
    
    * fix: sync files
    
    * fix: req parsing body
    
    * feat: add some merchant pages
    
    * feat: add menu page
    
    * fix: ignore sw.js.map
    
    * feat: add menu page
    
    * feat: add product image
    
    * feat: add menu model
    
    * fix: add menu api
    
    ---------
    
    Co-authored-by: Nur Aziz Tri Indrawan <indraaziznur@gmail.com>

--- pages/merchant/katalog/add.tsx
+++ pages/merchant/katalog/add.tsx
@@ -3,7 +3,7 @@ import Input from "components/elements/Input";
 import MerchantLayout from "components/layout/MerchantLayout";
 import Image from "next/image";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface IFormInputs {
   namaProduk: string;
@@ -13,11 +13,9 @@ interface IFormInputs {
   stok: number;
 }
 
-const Add: React.FC = (): JSX.ELement => {
+const Add: React.FC = (): JSX.Element => {
   const [currentFile, setCurrentFile] = useState<File | null>(null);
-  const [previewImage, setPreviewImage] = useState<string | ArrayBuffer | null>(
-    null
-  );
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
 
   const {
     register,
@@ -25,10 +23,38 @@ const Add: React.FC = (): JSX.ELement => {
     formState: { errors },
   } = useForm<IFormInputs>();
 
+  const pickPicture = (e: any) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    if (file) {
+      setCurrentFile(file);
+      setPreviewImage(URL.createObjectURL(file));
+    }
+  };
+
   const submitHandler: SubmitHandler<IFormInputs> = async (
     data: IFormInputs
   ) => {
-    console.log(data);
+    const { namaProduk, harga, kategori, deskripsi, stok } = data;
+    if (currentFile) {
+      const formData = new FormData();
+      formData.append("file", currentFile);
+      formData.append("name", namaProduk);
+      formData.append("price", harga.toString());
+      formData.append("category", kategori);
+      formData.append("description", deskripsi);
+      formData.append("stock", stok.toString());
+
+      const res = await fetch(
+        "/api/merchant//add",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const json = await res.json();
+      console.log(json);
+    }
   };
 
   return (
@@ -39,7 +65,24 @@ const Add: React.FC = (): JSX.ELement => {
         </h1>
         <h1 className="text-center mb-2">Foto Produk</h1>
         <form onSubmit={handleSubmit(submitHandler)}>
-          <input type="file" id="picture" className="hidden" />
+          {previewImage ? (
+            <div className="rounded-full relative w-24 h-24 overflow-hidden">
+              <Image
+                src={previewImage}
+                alt="preview"
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+          ) : (
+            <div className="rounded-full w-24 h-24 bg-[#C4C4C4]"></div>
+          )}
+          <input
+            type="file"
+            id="picture"
+            className="hidden"
+            onChange={pickPicture}
+          />
           <label htmlFor="picture">
             <div className="relative h-32 w-32 mx-auto mb-2">
               <Image
@@ -99,7 +142,7 @@ const Add: React.FC = (): JSX.ELement => {
             className="mb-3"
             error={errors.stok}
           />
-          <Button text="Tambahkan" isSubmit={true}/>
+          <Button text="Tambahkan" isSubmit={true} />
         </form>
       </div>
     </MerchantLayout>
