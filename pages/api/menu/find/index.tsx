67d6b0f76b1807f18b7cf8b97b74c867ce706c66
LatianOTI <ccobaabocc00@gmail.com>
2023-03-22 06:46:32 +0700

    add menu, fetch data katalog merchant

--- pages/api/menu/find/index.tsx
+++ pages/api/menu/find/index.tsx
@@ -5,14 +5,11 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    const { id } = req.query;
+  if (req.method === "POST") {
+    console.log("GET");
+    // const { id } = req.query;
 
-    const menu = await prisma.menu.findUnique({
-      where: {
-        id: parseInt(id as string),
-      },
-    });
+    const menu = await prisma.menu.findMany({});
 
     if (!menu) {
       return res.status(404).json({ message: "Menu not found" });
--- pages/merchant/katalog/index.tsx
+++ pages/merchant/katalog/index.tsx
@@ -5,19 +5,20 @@ import Searchbar from "components/elements/Searchbar";
 import MerchantLayout from "components/layout/MerchantLayout";
 import { getSession, useSession } from "next-auth/react";
 import { useState } from "react";
+import prisma from "lib/prisma";
 
-const Katalog: React.FC = (): JSX.Element => {
+const Katalog = (props: {menu: any}): JSX.Element => {
   const { data: session, status } = useSession();
   // console.log(session?.user);
   const user = session?.user;
 
-  console.log(user?.name);
+  // console.log(props);
 
   const [enabled, setEnabled] = useState(false);
   return (
     <MerchantLayout location="katalog">
       <div className="flex flex-col gap-4 min-h-screen px-4 py-9">
-        <h1 className="font-extrabold mb-2">Preksu: Ayam Geprek & Susu</h1>
+        <h1 className="font-extrabold mb-2">{session?.user.name}</h1>
         <div className="flex justify-between">
           <div className="w-44 h-8 bg-c-orange-100 flex rounded-full justify-center items-center shadow-[0_3px_3px_0.1px_rgb(400,100,0,0.3),inset_0_3px_7px_6px_rgb(500,500,500,0.2)]">
             <p className="text-c-orange-700 text-sm">Customer View</p>
@@ -69,10 +70,15 @@ const Katalog: React.FC = (): JSX.Element => {
           </Menu>
         </div>
         <Searchbar />
+        {props?.menu.map((item: any) => {
+          return (
+            <ItemMerchant title={item.name} price={item.price} description={item.description} stock={item.stock} />
+          )
+        })}
+        {/* <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} />
         <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} />
         <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} />
-        <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} />
-        <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} />
+        <ItemMerchant title={"Title Title tit"} price={10000} description={""} stock={0} /> */}
 
         <Button text="+" href="/merchant/katalog/add" size="small" className="fixed right-4 bottom-20" />
       </div>
@@ -82,9 +88,9 @@ const Katalog: React.FC = (): JSX.Element => {
 
 export default Katalog;
 
-export const getServerSideProps = async ({req}) => {
+export const getServerSideProps = async ({req}:{req: any}) => {
   const session = await getSession({req});
-  // console.log(session);
+  console.log(session?.user.id);
   if (!session) {
     return {
       redirect: {
@@ -93,7 +99,14 @@ export const getServerSideProps = async ({req}) => {
       },
     };
   }
+
+  const data = await prisma.menu.findMany({
+    where: {
+      ownerId: session?.user.id
+    }
+  });
+
   return {
-    props: { session },
+    props: { menu: data },
   }
 }
