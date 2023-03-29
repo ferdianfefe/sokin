69ca0a96618db34ba38eb6ebb15cc256d64b6cd6
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-03-29 08:44:56 +0700

    Feature/add edit menu image (#64)
    * feat: add get and delete menu
    
    * feat: add and edit menu image
    
    * feat: add menu popup

--- components/elements/ItemCustomer/index.tsx
+++ components/elements/ItemCustomer/index.tsx
@@ -8,18 +8,34 @@ const ItemCustomer: React.FC<{
   description: string;
   image?: string;
   className?: string;
-}> = ({ title, price, image, className }): JSX.Element => {
-    const [count, setCount] = useState(0);
-    return (
-    <div className={`${className} flex flex-col items-center rounded-2xl p-2 h-64 shadow-[-2px_1px_4px_2px_rgb(300,75,0,0.13)]`}>
-        <Image src={image || `https://cdn-icons-png.flaticon.com/512/1205/1205761.png`} width={147} height={147} alt={""}  className="rounded-xl"/>
-        <div className="mt-3">
-            <h3 className="font-bold">{title}</h3>
-            <p className="text-sm font-semibold">Rp{price}</p>
-        <Button text="Tambah" size="very small" className="mt-3"/>
-        </div>
+  onAddMenu?: () => void;
+}> = ({
+  title,
+  price,
+  image,
+  className,
+  description,
+  onAddMenu = () => {},
+}): JSX.Element => {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      className={`${className} flex flex-col items-center rounded-2xl p-2 h-64 shadow-[-2px_1px_4px_2px_rgb(300,75,0,0.13)]`}
+    >
+      <Image
+        src={image || `https://cdn-icons-png.flaticon.com/512/1205/1205761.png`}
+        width={147}
+        height={147}
+        alt={""}
+        className="rounded-xl"
+      />
+      <div className="mt-3">
+        <h3 className="font-bold">{title}</h3>
+        <p className="text-sm font-semibold">Rp{price}</p>
+        <Button text="Tambah" size="very small" className="mt-3" onClickHandler={() => onAddMenu(title, price, image, description)}/>
+      </div>
     </div>
-    );
+  );
 };
 
 export default ItemCustomer;
--- components/layout/MerchantLayout/index.tsx
+++ components/layout/MerchantLayout/index.tsx
@@ -1,14 +1,14 @@
 // Create a layout with navbar
 import Navbar from "components/elements/Navbar";
 
-const MerchantLayout: React.FC<{ children: any, location: string }> = ({
+const MerchantLayout: React.FC<{ children: any; location: string }> = ({
   children,
   location,
 }): JSX.Element => {
   return (
     <div className="relative h-screen">
       {children}
-      <Navbar location={location} role='merchant'/>
+      <Navbar location={location} role="merchant" className="z-[100]" />
     </div>
   );
 };
