1d08bd4303ea823dcd8873bacb3c9bcd6f6be3ec
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-03-29 08:44:31 +0700

    feat: add menu popup

--- pages/pesan/merchant/index.tsx
+++ pages/pesan/merchant/index.tsx
@@ -19,52 +19,69 @@ const Merchant = (props: { menu: any }): JSX.Element => {
   const [keyword, setKeyword] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [similar, setSimilar] = useState([]);
+  const [addMenuIsOpen, setAddMenuIsOpen] = useState(false);
+  const [addedMenuData, setAddedMenuData] = useState({
+    title: "",
+    price: 0,
+    image: "",
+  });
 
   const { data: session, status } = useSession();
-  // console.log(session?.user);
   const user = session?.user;
 
-  // console.log(router.query);
+  const clickAddMenuHandler = (
+    title: string,
+    price: number,
+    image: string,
+    description: string
+  ) => {
+    setAddMenuIsOpen(true);
+    setAddedMenuData({ title, price, image, description });
+  };
 
   useEffect(() => {
     fetch("/api/profile/merchant", {
       method: "POST",
       body: JSON.stringify({ id: router.query.id }),
-    }).then((res) => res.json()).then((data) => {
-      setMenu(data.data);
-      setName(data.name);
-      setLogo(data.logo);
     })
+      .then((res) => res.json())
+      .then((data) => {
+        setMenu(data.data);
+        setName(data.name);
+        setLogo(data.logo);
+      });
   }, []);
 
   const [customerView, setcustomerView] = useState(false);
 
   const change = (e: any) => {
     setKeyword(e.target.value);
-    // console.log(keyword);
-  }
+  };
 
   const search = async (e: any) => {
     if (e.key === "Enter" || e === "Enter") {
-      // setFixKeyword(keyword);
-      // Router.push(`/search?keyword=${keyword}`);
-      console.log(keyword);
       e.target.value = "";
       setKeyword("");
       await fetch(`/api/search`, {
         method: "POST",
-        body: JSON.stringify({keyword: keyword})
-      }).then(res => res.json()).then(data => {
-        console.log(data.data);
-        setSearchResult(data.data);
-        setSimilar(data.data2);
+        body: JSON.stringify({ keyword: keyword }),
       })
+        .then((res) => res.json())
+        .then((data) => {
+          setSearchResult(data.data);
+          setSimilar(data.data2);
+        });
     }
-  }
+  };
 
   return (
     <MerchantLayout location="katalog">
       <div className="min-h-screen">
+        <AddMenuPopUp
+          show={addMenuIsOpen}
+          setAddMenuIsOpen={setAddMenuIsOpen}
+          menuData={addedMenuData}
+        />
         <div className="flex flex-col gap-4 px-4 pt-9">
           <h1 className="font-extrabold mb-2">{name}</h1>
         </div>
@@ -111,19 +128,19 @@ const Merchant = (props: { menu: any }): JSX.Element => {
           </div>
         </div>
         <div className="mt-4 px-4">
-        <div className="relative flex">
-              {/* ICON */}
-              <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                <Image src={Search} alt={""} />
-              </span>
-              <input
-                placeholder="Cari makananmu"
-                type="text"
-                className="bg-[#FE8304] rounded-full bg-opacity-40 w-full py-[6px] px-14 font-bold text-sm placeholder-[#817A7A] bg-auto focus:outline-none"
-                onChange={change}
-                onKeyDown={search}
-              ></input>
-            </div>
+          <div className="relative flex">
+            {/* ICON */}
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <Image src={Search} alt={""} />
+            </span>
+            <input
+              placeholder="Cari makananmu"
+              type="text"
+              className="bg-[#FE8304] rounded-full bg-opacity-40 w-full py-[6px] px-14 font-bold text-sm placeholder-[#817A7A] bg-auto focus:outline-none"
+              onChange={change}
+              onKeyDown={search}
+            ></input>
+          </div>
         </div>
         <div className="flex flex-col gap-4 px-4 py-6">
           <div className="flex justify-end">
@@ -153,16 +170,18 @@ const Merchant = (props: { menu: any }): JSX.Element => {
             </Menu>
           </div>
           <div className={"grid grid-cols-2 gap-4"}>
-            {menu.length != 0 && menu.map((item: any, index: number) => (
-              <div className="" key={index}>
+            {menu.length != 0 &&
+              menu.map((item: any, index: number) => (
+                <div className="" key={index}>
                   <ItemCustomer
+                    onAddMenu={clickAddMenuHandler}
                     title={item.name}
                     price={item.price}
                     description={item.description}
                     image={item.image}
                   />
-              </div>
-            ))}
+                </div>
+              ))}
             {/* {props?.menu?.map((item: any, index: number) => {
               return (
                 <div className="" key={index}>
@@ -187,6 +206,83 @@ const Merchant = (props: { menu: any }): JSX.Element => {
 };
 
 export default Merchant;
+
+const AddMenuPopUp = ({
+  show,
+  setAddMenuIsOpen = () => {},
+  menuData,
+}: {
+  show: Boolean;
+  setAddMenuIsOpen: Function;
+  menuData: object;
+}): JSX.Element => {
+  const [itemCount, setItemCount] = useState(1);
+
+  return (
+    <div
+      className={`top-0 left-0 fixed w-screen h-screen z-[99] ${
+        show == true ? "block" : "hidden"
+      } `}
+    >
+      <div
+        className="bg-white bg-opacity-50 w-full h-full top-0 left-0 sticky"
+        onClick={() => {
+          setAddMenuIsOpen(false);
+        }}
+      ></div>
+      <div className="bg-white sticky bottom-0 left-0 w-full h-[26rem] pt-4 px-8">
+        <h1 className="font-bold text-center mb-6">Tambah Menu</h1>
+        <div className="flex mb-6">
+          <div className="relative w-40 h-40 flex-1">
+            <Image
+              src={menuData.image}
+              alt="menuImage"
+              fill
+              className="object-cover rounded-3xl"
+            />
+          </div>
+          <div className="ml-3 flex-1">
+            <h1 className="font-bold">{menuData.title}</h1>
+            <h1 className="font-bold">Rp {menuData.price}</h1>
+            <small className="text-[#ADADAD]">{menuData.description}</small>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="font-bold">Jumlah Pesanan</p>
+          <div className="flex items-center gap-4">
+            <Image
+              src={"/images/icons/minus-icon.svg"}
+              alt="minusIcon"
+              width={40}
+              height={40}
+              onClick={() => {
+                if (itemCount > 1) {
+                  setItemCount(itemCount - 1);
+                }
+              }}
+            />
+            <p>{itemCount}</p>
+            <Image
+              src={"/images/icons/plus-icon.svg"}
+              alt="plusIcon"
+              width={40}
+              height={40}
+              onClick={() => {
+                setItemCount(itemCount + 1);
+              }}
+            />
+          </div>
+        </div>
+        <div className="">
+          <Button
+            text={`Tambah Pesanan - Rp ${menuData.price * itemCount}`}
+            className="font-bold"
+          ></Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // const SearchBar: React.FunctionComponent = (): JSX.Element => {
 //   return (
