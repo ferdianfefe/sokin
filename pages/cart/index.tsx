5326f107b7d737d8c6653c0ddf10ab0c9d4b13c5
Nur Aziz Tri Indrawan <88185134+Poor1ST@users.noreply.github.com>
2023-05-09 23:33:19 +0700

    Feature/fix cart (#70)
    * fix:checkout
    
    * fix:cart

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -3,7 +3,7 @@ import DefaultLayout from "components/layout/DefaultLayout";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type CartContentProps = {
   restaurantName: String;
