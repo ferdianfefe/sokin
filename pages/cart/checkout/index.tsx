3d64a955a3a4f7fc7b67ac0c1ac6cad4d3e6772d
Nur Aziz Tri Indrawan <indraaziznur@gmail.com>
2023-05-09 23:32:33 +0700

    fix:cart

--- pages/cart/checkout/index.tsx
+++ pages/cart/checkout/index.tsx
@@ -31,7 +31,9 @@ const Checkout: React.FC = (): JSX.Element => {
     deliveryTime: 30,
   });
   const [show, setShow] = useState(false);
-  
+  const [showOpsi, setShowOpsi] = useState(false);
+  const [opsiPembayaran, setOpsiPembayaran] = useState("Soket");
+
   const [cartContent, setCartContent] = useState<CartContentProps[]>([
     {
       restaurantName: "McDonalds",
@@ -45,28 +47,42 @@ const Checkout: React.FC = (): JSX.Element => {
   useEffect(() => {
     fetch("http://localhost:3000/api/promo", {
       method: "GET",
-    }).then((res) => 
-      res.json()
-    ).then((data) => {
-      setPromo(data)
     })
-  }, [])
+      .then((res) => res.json())
+      .then((data) => {
+        setPromo(data);
+      });
+  }, []);
 
   const showPromo = () => {
-    setShow(true)
-  }
+    setShow(true);
+  };
 
   const hidePromo = () => {
-    setShow(false)
-  }
+    setShow(false);
+  };
+
+  const toggleShowOpsi = () => {
+    setShowOpsi(!showOpsi);
+  };
+
+  const opsiSoket = () => {
+    setOpsiPembayaran("Soket");
+  };
+
+  const opsiCash = () => {
+    setOpsiPembayaran("Cash");
+  };
 
   return (
-    <DefaultLayout location="pesan">
+    <DefaultLayout location="pesan" className="z-60">
       {show && (
         <div className="w-full min-h-full z-10 bg-[#FFFFFF] fixed">
           <div className="flex items-center px-6 pt-6 mb-4">
             <h1 className="flex items-center text-2xl font-bold text-neutral-700 ml-4">
-              <p className="hover:cursor-pointer" onClick={hidePromo}>X</p>
+              <p className="hover:cursor-pointer" onClick={hidePromo}>
+                X
+              </p>
               {/* <p>-</p> */}
             </h1>
             <h1 className="text-2xl font-semibold text-neutral-700 ml-4">
@@ -75,33 +91,33 @@ const Checkout: React.FC = (): JSX.Element => {
           </div>
           <div className="px-[5%] font-bold">
             <p className="mb-4">Promo Ongkos Kirim</p>
-            {promo.length > 0 && (promo.map((item) => {
-              if (item.promoType === "ongkir"){
-                return (
-                  <div className="w-full h-[25vw] bg-[url('/ongkirPromo.png')] bg-contain bg-no-repeat text-[#FFFFFF] px-[30%] pt-[1.5vh]">
-                    <p>{item.title}</p>
-                    <p className="text-lg font-light">- Sampai dengan</p>
-                    <p className="text-lg font-light">- Minimal order</p>
-                  </div>
-                )
-              }
-            })
-            )}
+            {promo.length > 0 &&
+              promo.map((item) => {
+                if (item.promoType === "ongkir") {
+                  return (
+                    <div className="w-full h-[25vw] bg-[url('/ongkirPromo.png')] bg-contain bg-no-repeat text-[#FFFFFF] px-[30%] pt-[1.5vh]">
+                      <p>{item.title}</p>
+                      <p className="text-lg font-light">- Sampai dengan</p>
+                      <p className="text-lg font-light">- Minimal order</p>
+                    </div>
+                  );
+                }
+              })}
           </div>
           <div className="px-[5%] font-bold">
             <p className="mb-4">Promo Makanan atau Minuman</p>
-            {promo.length > 0 && (promo.map((item) => {
-              if (item.promoType === "makanan"){
-                return (
-                  <div className="w-full h-[25vw] bg-[url('/foodPromo.png')] bg-contain bg-no-repeat text-[#FE8304] px-[30%] pt-[1.5vh]">
-                    <p>{item.title}</p>
-                    <p className="text-lg font-light">- Sampai dengan</p>
-                    <p className="text-lg font-light">- Minimal order</p>
-                  </div>
-                )
-              }
-            })
-            )}
+            {promo.length > 0 &&
+              promo.map((item) => {
+                if (item.promoType === "makanan") {
+                  return (
+                    <div className="w-full h-[25vw] bg-[url('/foodPromo.png')] bg-contain bg-no-repeat text-[#FE8304] px-[30%] pt-[1.5vh]">
+                      <p>{item.title}</p>
+                      <p className="text-lg font-light">- Sampai dengan</p>
+                      <p className="text-lg font-light">- Minimal order</p>
+                    </div>
+                  );
+                }
+              })}
           </div>
         </div>
       )}
@@ -159,7 +175,10 @@ const Checkout: React.FC = (): JSX.Element => {
               <ItemBox key={index} item={item} index={index} />
             ))}
           </div>
-          <div className="bg-c-orange-200 flex items-center justify-evenly py-2 px-3 rounded-3xl mb-8 shadow-[0_3px_3px_0.1px_rgb(400,100,0,0.3),inset_0_3px_6px_10px_rgb(500,500,500,0.3)]" onClick={showPromo}>
+          <div
+            className="bg-c-orange-200 flex items-center justify-evenly py-2 px-3 rounded-3xl mb-8 shadow-[0_3px_3px_0.1px_rgb(400,100,0,0.3),inset_0_3px_6px_10px_rgb(500,500,500,0.3)]"
+            onClick={showPromo}
+          >
             <div className="relative w-8 h-8">
               <Image
                 src="/images/icons/discount.svg"
@@ -213,8 +232,7 @@ const Checkout: React.FC = (): JSX.Element => {
             </div>
           </div>
         </div>
-        <PaymentPopup />
-        <div className="w-full h-40 bg-c-orange-300 px-7 pb-3 pt-6 shadow-[inset_0_0px_15px_7px_rgb(500,500,500,0.2)]">
+        {/* <div className="w-full h-40 bg-c-orange-300 px-7 pb-3 pt-6 shadow-[inset_0_0px_15px_7px_rgb(500,500,500,0.2)]">
           <div className="flex justify-between mb-8">
             <div className="flex">
               <Image src={"/images/icons/SocketOrange.svg"} alt={""} width={41} height={31}/>
@@ -226,9 +244,17 @@ const Checkout: React.FC = (): JSX.Element => {
             <div className="w-20 h-6 bg-c-orange-400"></div>
           </div>
           <Button text={"Pesan dan Bayar Sekarang"} type="gray"/>
-        </div>
+        </div> */}
       </div>
-      <PaymentPopup />
+      <PaymentPopup toggleShowOpsi={toggleShowOpsi} />
+      {showOpsi && (
+        <PopUpOpsi
+          toggleShowOpsi={toggleShowOpsi}
+          opsiSoket={opsiSoket}
+          opsiCash={opsiCash}
+          opsiPembayaran={opsiPembayaran}
+        />
+      )}
     </DefaultLayout>
   );
 };
@@ -260,7 +286,11 @@ const ItemBox: React.FC = ({
   );
 };
 
-const PaymentPopup: React.FC = (): JSX.Element => {
+const PaymentPopup: React.FC = ({
+  toggleShowOpsi,
+}: {
+  toggleShowOpsi: Function;
+}): JSX.Element => {
   const [saldo, setSaldo] = useState(0);
   return (
     <div className="sticky bottom-0 left-0">
@@ -271,26 +301,75 @@ const PaymentPopup: React.FC = (): JSX.Element => {
         <p className="text-neutral-50">Saldo kurang, Top up atau bayar tunai</p>
         <Button text="Top up" className="!w-16 !h-8 text-base font-semibold" />
       </div>
-      <div className="bg-c-orange-200">
+      <div className="bg-c-orange-200 px-7">
         <div className="flex items-center justify-between py-8">
-          <div className="flex">
-            <div className="relative h-6 w-6">
-              <Image src="/images/icons/wallet.svg" alt="wallet-icon" fill />
+          <div className="flex items-center">
+            <div className="relative">
+              <Image
+                src="/images/icons/wallet.svg"
+                alt="wallet-icon"
+                width={41}
+                height={31}
+              />
             </div>
-            <div className="flex-2">
-              <p>Soket</p>
-              <p>Rp {100000}</p>
+            <div className="flex-2 ml-4">
+              <p className="font-semibold">Soket</p>
+              <p className="font-bold text-c-red-700">Rp {100000}</p>
             </div>
           </div>
-          <Button
-            text="Ganti Opsi"
-            className="!w-20 bg-c-orange-200 text-base font-light !text-c-orange-800"
-          />
+          <div onClick={toggleShowOpsi}>
+            <Button
+              text="Ganti Opsi"
+              className="!w-20 bg-c-orange-300 text-base font-light !text-c-orange-800"
+            />
+          </div>
         </div>
         <Button
           text="Pesan dan Bayar Sekarang"
-          className="bg-neutral-600 font-semibold"
+          className="bg-neutral-600 font-semibold mb-2"
         />
+      </div>
+    </div>
+  );
+};
+
+const PopUpOpsi = ({
+  toggleShowOpsi,
+  opsiSoket,
+  opsiCash,
+  opsiPembayaran,
+}: {
+  toggleShowOpsi: () => void;
+  opsiSoket: () => void;
+  opsiCash: () => void;
+  opsiPembayaran: String;
+}) => {
+  return (
+    <div className="fixed inset-0 flex flex-col justify-end z-10 backdrop-blur-sm">
+      <div className="w-full h-[60%]" onClick={toggleShowOpsi}></div>
+      <div className="bg-white w-full h-[40%] rounded-t-[12px] shadow-2xl py-5 px-6">
+        <div className="flex mb-6" onClick={toggleShowOpsi}>
+          <Image src={"/images/icons/left-arrow.svg"} alt={""} width={26} height={26} />
+          <h2 className="font-bold text-3xl ml-8 text-gray-500">Pilih Opsi Pembayaran</h2>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className={`${opsiPembayaran === "Soket" ? "border-[2px] border-c-orange-600" : ""} flex px-4 items-center justify-between w-80 h-16 rounded-2xl mb-8 shadow-[0_1px_4px_2px_rgb(400,100,0,0.2)]`}>
+            <Image src={"/images/SoketCircle.svg"} width={46} height={46}/>
+            <div className="flex-2 mr-4">
+              <h3 className="font-semibold">Soket</h3>
+              <p className="text-sm">saldo: <span className="text-c-orange-700 font-semibold">Rp20.000</span></p>
+            </div>
+            <Button text="Pilih" className="!w-16 !h-6" onClickHandler={opsiSoket}/>
+          </div>
+          <div className={`${opsiPembayaran === "Cash" ? "border-[2px] border-c-orange-600" : ""} flex px-4 items-center justify-between w-80 h-16 rounded-2xl mb-8 shadow-[0_1px_4px_2px_rgb(400,100,0,0.2)]`}>
+            <Image src={"/images/CashCircle.svg"} width={46} height={46}/>
+            <div className="flex-2">
+              <h3 className="font-semibold">Cash</h3>
+              <p className="text-xs font-semibold">Jangan lupa siapin uang pas ya</p>
+            </div>
+            <Button text="Pilih" className="!w-16 !h-6" onClickHandler={opsiCash}/>
+          </div>
+        </div>
       </div>
     </div>
   );
