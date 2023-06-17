ffdc1d92e61a2a25e76b6217f1a8db71fda890b6
Richardus Ferdian <50134930+ferdianfefe@users.noreply.github.com>
2023-06-17 21:34:57 +0700

    feat: update user coordinates

--- pages/api/profile/user/index.ts
+++ pages/api/profile/user/index.ts
@@ -1,15 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === "GET") {
-        const user = await prisma.owner.findFirst();
-        return res.status(200).json(user);
-    }
-
-    if (req.method === "PATCH") {
-        const {coordinates} = req.body;
-        const userId = 
-    }
-    
-    return res.status(405).json({ message: "Method unallowed" });
-}
+  if (req.method === "GET") {
+    const user = await prisma.owner.findFirst();
+    return res.status(200).json(user);
+  }
+  return res.status(405).json({ message: "Method unallowed" });
+};
